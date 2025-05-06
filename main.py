576836a0e895dd1e72bbd5e023f8a073141b5e37
DVanam <hisokadara@gmail.com>
2025-05-06 15:26:39 +0300

    task-directory creating

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import sys
+# import requests
+
+python_path = os.path.dirname(sys.executable)
+
+python_version = sys.version
+
+def main():
+  pass
+  # return requests.get(url='https://google.com')
+
+# В терминал .\main.py
+print(f'ПИТЬ К ИНТЕРПРЕТАТОРУ {python_path}. ВЕРСИЯ {python_version}')
+
+if __name__ == '__main__':
+  print(main())
