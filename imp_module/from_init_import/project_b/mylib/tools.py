8cec9a99b7daf79619b6ee33a80203a5b0bdac63
DVanam <hisokadara@gmail.com>
2025-07-18 15:20:57 +0300

    Sint-manual/tasc/- Create new derictory. Add 15 tascs

--- imp_module/from_init_import/project_b/mylib/tools.py
+++ imp_module/from_init_import/project_b/mylib/tools.py
@@ -1,7 +1,10 @@
 # 1 Определить, является ли строка палиндромом (читается одинаково слева направо и справа налево
 # word_str = "radar"
-
+word_str = "rabar"
 def paly(s):
   st = s.lower().replace(" ", "")
   return st == st[::-1]
-# print(paly(word_str))
+print(paly(word_str))
+
+if __name__ == "__main__":
+  paly()
